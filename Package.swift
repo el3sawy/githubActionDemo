d10b02d7025cd4b8093c75a49656afeb9ba78831
Ahmed Naguib <ahmed.nageeb@squadio.com>
2026-06-21 17:09:25 +0300

    ewewqewq

--- Package.swift
+++ Package.swift
@@ -3,10 +3,20 @@ import PackageDescription
 
 let package = Package(
     name: "Danger",
+    defaultLocalization: "en",
+    products: [
+        .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"])
+    ],
     dependencies: [
         .package(
             url: "https://github.com/danger/swift.git",
             from: "3.0.0"
+        )
+    ],
+    targets: [
+        .target(
+            name: "DangerDependencies",
+            dependencies: [.product(name: "Danger", package: "swift")]
         )
     ]
 )
